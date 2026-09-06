a44a39649af292b39106df93ae47fcb38209622f
Saurabh Yadav <eryadav001@gmail.com>
2026-09-06 22:11:05 +0530

    Added new code

---
+++ Operators/modules.py
@@ -0,0 +1,22 @@
+# math
+
+import math
+num = int(input("Give a Number: "))
+result = math.factorial(num)
+print("Factorial is: ", result)
+
+# Keyword
+
+import keyword
+print(keyword.kwlist)
+
+# Random
+
+import random
+print(random.randint(1, 100))
+
+# Datetime
+
+import datetime
+print(datetime.datetime.now())
+
