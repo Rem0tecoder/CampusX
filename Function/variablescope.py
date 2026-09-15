4ee30c1676d89fe89d9085a078ad61738b105513
Saurabh Yadav <eryadav001@gmail.com>
2026-09-15 10:43:52 +0530

    New Commit

--- Function/variablescope.py
+++ Function/variablescope.py
@@ -21,3 +21,13 @@ def h(y):
 x = 5
 h(x)
 print(x)
+
+def s(x):
+    x = x + 1
+    print('in s(x): x =', x)
+    return x
+
+x = 3
+z = s(x)
+print(' in main program scope: z =', z)
+print('in main program scope: x =', x)
